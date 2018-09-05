799979d81b39946a02d7d55a4c70e8183931d0c9
Nicholas Rodine <halfofastaple@gmail.com>
2018-09-04 22:28:47 -0400

    Removed logging.

--- MagicMod.ts
+++ MagicMod.ts
@@ -3,15 +3,12 @@ import { IStatDisplayDescription } from "newui/screen/screens/game/static/stats/
 import { StatDisplayType } from "entity/IStats";
 import IPlayer from "player/IPlayer";
 import { HookMethod } from "mod/IHookHost";
-import Log from "utilities/Log";
 import { SpellbookMenuBarButton } from "./Spells/SpellbookMenuBarButton";
 import { SpellbookDialog } from "./Spells/SpellbookDialog";
 import Register from "mod/ModRegistry";
 import { DialogId } from "newui/screen/screens/game/Dialogs";
 import { ManaProvider } from "./ManaProvider";
 import { IMagicMod } from "./IMagicMod";
-
-let log : Log;
 
 export default class MagicMod extends Mod implements IMagicMod {
 	public manaProvider: ManaProvider;
@@ -20,8 +17,6 @@ export default class MagicMod extends Mod implements IMagicMod {
 	public spellbookDialogId : DialogId;
 
 	public onInitialize(): void {
-		log = this.getLog();
-
 		this.manaProvider = new ManaProvider(this.addStat("Mana", new ManaStatDescription() ));
 		
 		let binding = this.addBindable("spellbook", { key: "KeyB"});
@@ -30,13 +25,11 @@ export default class MagicMod extends Mod implements IMagicMod {
 
 	@HookMethod
 	public onGameStart(isLoadingSave: boolean, playedCount: number): void {
-		log.info("game start: " + localPlayer.name)
 		this.manaProvider.enableMana(localPlayer);
 	}
 	
 	@HookMethod
 	onPlayerJoin(player: IPlayer): void{
-		log.info("player joined: " + player.name) 
 		this.manaProvider.enableMana(player);
 	}
 }
